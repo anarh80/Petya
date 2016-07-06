0cf1a6de62b35f9f4c63063437c31aa5dc876356
aaxon <alexandr.axon@gmail.com>
2016-07-06 13:00:23 +0300

    first steps

---
+++ Assets/WebClient/EbayAPI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EbayAPI : MonoBehaviour {
+
+	public Text resultText;
+
+	public void GetResponce(){
+		
+	}
+
+}
